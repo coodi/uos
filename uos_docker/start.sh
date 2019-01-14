454b212d69111371bde8e143e120c841360a420e
Radich <pit_radich@mail.ru>
2019-01-14 14:08:05 +0100

    fix nodeos exec in docker

--- uos_docker/start.sh
+++ uos_docker/start.sh
@@ -8,14 +8,14 @@ for i in $@; do
 done
 
 if [ -d /root/.local/share/eosio/nodeos/data ] && [ $fresh_chain -eq 0  ]; then
-  /usr/local/bin/nodeos "$@"
+  exec /usr/local/bin/nodeos "$@"
 else
   if [ ! -f /root/.local/share/eosio/nodeos/config/config.ini ]; then 
     mkdir -p /root/.local/share/eosio/nodeos/config
-    wget -O  https://raw.githubusercontent.com/UOSnetwork/uos.docs/master/testnetv1/config.ini
+    wget -O /root/.local/share/eosio/nodeos/config/config.ini https://raw.githubusercontent.com/UOSnetwork/uos.docs/master/testnetv1/config.ini
   fi
   if [ ! -f /root/.local/share/eosio/nodeos/config/genesis.json ]; then 
     wget -O /root/.local/share/eosio/nodeos/config/genesis.json https://raw.githubusercontent.com/UOSnetwork/uos.docs/master/testnetv1/genesis.json
   fi
-  /usr/local/bin/nodeos --genesis-json /root/.local/share/eosio/nodeos/config/genesis.json "$@"
+  exec /usr/local/bin/nodeos --genesis-json /root/.local/share/eosio/nodeos/config/genesis.json "$@"
 fi
